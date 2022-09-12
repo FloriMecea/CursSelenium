cf1129237bfda1a8f9fea57f2facb2af9f169bbf
Florentina Manescu <florentina.manescu@softelligence.net>
2022-09-12 10:51:25 +0300

    curs7

---
+++ src/main/java/curs7/NavigationExample.java
@@ -0,0 +1,22 @@
+package curs7;
+
+import org.testng.annotations.Test;
+
+import utils.BaseTest;
+
+public class NavigationExample extends BaseTest {
+
+	@Test
+	public void navigateExamples() throws InterruptedException {
+		Thread.sleep(2000);
+		driver.get("https://emag.ro");
+		Thread.sleep(2000);
+		driver.navigate().back(); //ma duce iar pe booklovers
+		driver.navigate().forward();
+		Thread.sleep(2000);
+		driver.navigate().to("https://altex.ro");
+		driver.navigate().refresh();
+	}
+	
+	
+}
