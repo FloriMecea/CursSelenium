2f855687e3aa1928e8d5441ddf230d39c34a788f
Florentina Manescu <florentina.manescu@softelligence.net>
2022-09-19 09:25:26 +0300

    curs9 login test

--- src/main/java/pages/LoginPage.java
+++ src/main/java/pages/LoginPage.java
@@ -1,5 +1,12 @@
 package pages;
 
+import org.openqa.selenium.WebDriver;
+
 public class LoginPage {
+	public WebDriver driver;
+	
+	public LoginPage(WebDriver driver) {
+		this.driver=driver;
+	}
 
 }
