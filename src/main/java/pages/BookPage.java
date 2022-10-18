0a70ed2f2e0db197243f71d462a72ceb91ce0e17
Florentina Manescu <florentina.manescu@softelligence.net>
2022-10-18 23:26:06 +0300

    tema curs11 selenium

---
+++ src/main/java/pages/BookPage.java
@@ -0,0 +1,32 @@
+package pages;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+
+public class BookPage {
+	public WebDriver driver;
+	public By booksPage= By.xpath("//a[contains(@href,'let-the-sun-shine') and contains(text(),'Let the sun shine')]");
+	
+	public By title= By.xpath("//h1[contains(@class,'entry-title')]");
+	public By stars= By.xpath("//p[@class='stars']");
+	public By price= By.xpath("//p[contains(@class,'price')]");
+	public By search= By.xpath("//a[contains(@class,'trigger')]");
+	public By shortDescription= By.xpath("//div[contains(@class,'short-description')]");
+	public By longDescription= By.xpath("//div[contains(@class,'product_meta')]");
+	public By quantity= By.xpath("//input[contains(@id,'quantity')]");
+	public By addToCart= By.xpath("//button[contains(@name,'add-to-cart')]");
+	
+	
+	
+	public BookPage(WebDriver driver) {
+		this.driver=driver;
+	}
+	
+	
+	public WebElement FindElement(By locator) {
+		   WebElement element=driver.findElement(locator);
+		  return element;
+			
+	}
+}
---
+++ src/main/java/tests/BookInformations_Tema11.java
@@ -0,0 +1,90 @@
+package tests;
+
+import static org.testng.Assert.assertEquals;
+import static org.testng.Assert.assertTrue;
+
+import org.openqa.selenium.By;
+import org.testng.annotations.Test;
+
+import pages.BookPage;
+import pages.NavMenuPage;
+import pages.ShopPage;
+import utils.BaseTest;
+
+public class BookInformations_Tema11 extends BaseTest{
+	
+	@Test(priority=0)
+	public void GoToBook() throws InterruptedException {
+		NavMenuPage menu= new NavMenuPage(driver);
+		BookPage pag=new BookPage(driver);
+		//ShopPage shop= new ShopPage(driver);
+		
+	    menu.navigateTo(menu.shopLink);
+		Thread.sleep(1000);
+	    menu.navigateTo(pag.booksPage);
+		Thread.sleep(2000);
+		
+		
+	    assertEquals(driver.getCurrentUrl(),"https://keybooks.ro/shop/let-the-sun-shine/");
+	//    assertTrue(pag.FindElement(pag.title).isDisplayed());
+	//    assertTrue(pag.FindElement(pag.price).isDisplayed());
+	//    assertTrue(pag.FindElement(pag.stars).isDisplayed());  //nu avem 
+	}
+	
+	@Test(priority=1)
+	public void checkTitleIsDisplayed() throws InterruptedException {
+		BookPage pag=new BookPage(driver);
+	
+	    assertTrue(pag.FindElement(pag.title).isDisplayed());
+
+	}
+	
+	@Test(priority=2)
+	public void checkPriceIsDisplayed() throws InterruptedException {
+		BookPage pag=new BookPage(driver);
+	  
+	    assertTrue(pag.FindElement(pag.price).isDisplayed());
+	 
+	}
+	
+	@Test(priority=3)
+	public void checkStarsIsDisplayed() throws InterruptedException {
+		BookPage pag=new BookPage(driver);
+		    
+	    assertTrue(pag.FindElement(pag.stars).isDisplayed());  //nu avem 
+	}
+	@Test(priority=4)
+	public void checkSearchIsDisplayed() throws InterruptedException {
+		BookPage pag=new BookPage(driver);
+		    
+	    assertTrue(pag.FindElement(pag.search).isDisplayed()); 
+	}
+	
+	@Test(priority=5)
+	public void checkShortDescriptionIsDisplayed() throws InterruptedException {
+		BookPage pag=new BookPage(driver);
+		    
+	    assertTrue(pag.FindElement(pag.shortDescription).isDisplayed());  
+	}
+	
+	@Test(priority=6)
+	public void checkLongDescriptionIsDisplayed() throws InterruptedException {
+		BookPage pag=new BookPage(driver);
+		    
+	    assertTrue(pag.FindElement(pag.longDescription).isDisplayed());  
+	}
+	
+	@Test(priority=7)
+	public void checkQuantityIsDisplayed() throws InterruptedException {
+		BookPage pag=new BookPage(driver);
+		    
+	    assertTrue(pag.FindElement(pag.quantity).isDisplayed());   
+	}
+	
+	@Test(priority=8)
+	public void checkAddToCartIsDisplayed() throws InterruptedException {
+		BookPage pag=new BookPage(driver);
+		    
+	    assertTrue(pag.FindElement(pag.addToCart).isDisplayed());   
+	}
+}
