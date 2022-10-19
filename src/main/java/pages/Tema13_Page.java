91a78788da23dcb734978f8f2794e000eed1370a
Florentina Manescu <florentina.manescu@softelligence.net>
2022-10-20 00:16:13 +0300

    tema 13 selenium

---
+++ src/main/java/pages/Tema13_Page.java
@@ -0,0 +1,62 @@
+package pages;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.ui.Select;
+
+public class Tema13_Page {
+	public WebDriver driver;
+	public By dropdown =By.cssSelector("select[class='orderby']");
+	public By firstBook= By.cssSelector("ul[class='products columns-4'] li[class*='column-1_4']:nth-of-type(1)");
+	public By lastBook= By.cssSelector("ul[class='products columns-4'] li[class*='column-1_4']:last-of-type");
+	public By firstBookPrice= By.cssSelector("ul[class='products columns-4'] li[class*='column-1_4']:nth-of-type(1) bdi");
+	public By lastBookPrice= By.cssSelector("ul[class='products columns-4'] li[class*='column-1_4']:last-of-type bdi");
+	
+	
+	public Tema13_Page(WebDriver driver) {
+		this.driver=driver;
+	}
+
+	public void SelectPriceLowToHigh(By locator) {
+		WebElement element=driver.findElement(locator);
+		Select select=new Select(element);
+		select.selectByValue("price");
+	}
+	
+	public String CheckCurrentSelection(By locator) {
+		WebElement element=driver.findElement(locator);
+		Select select=new Select(element);
+		return select.getFirstSelectedOption().getText();
+	}
+	
+	public void Compare() {
+		WebElement price1= driver.findElement(firstBookPrice);		
+		WebElement price2= driver.findElement(lastBookPrice);
+		String text1=price1.getText();
+		
+		System.out.println(text1);
+		String text2=price2.getText();
+		System.out.println(text2);
+		
+		text1=text1.substring(1);
+		text2=text2.substring(1);
+		System.out.println("After eliminating $ from text1: "+ text1);
+		System.out.println("After eliminating $ from text2: "+ text2);
+		
+		float nr1=Float.parseFloat(text1);
+		//System.out.println(nr1);
+		float nr2=Float.parseFloat(text2);
+		//System.out.println(nr2);
+		
+		if(nr1<nr2) {
+			System.out.println("Prima carte are pretul mai mic decat ultima carte");
+	
+		} else if(nr1>nr2) {
+			System.out.println("Prima carte are pretul mai mare decat ultima carte");
+		
+		} else
+			 System.out.println("Cele 2 carti au acelasi pret");
+			
+	}
+}
---
+++ src/main/java/tests/Tema13_Test.java
@@ -0,0 +1,30 @@
+package tests;
+
+import static org.testng.Assert.assertEquals;
+
+import org.openqa.selenium.support.ui.Select;
+import org.testng.annotations.Test;
+
+import pages.NavMenuPage;
+import pages.Tema13_Page;
+import utils.BaseTest;
+
+public class Tema13_Test extends BaseTest {
+	
+	
+	@Test
+	public void comparePrice() {
+		NavMenuPage menu=new NavMenuPage(driver);
+		menu.navigateTo(menu.shopLink);
+		
+		Tema13_Page pag= new Tema13_Page(driver);
+		
+		assertEquals(driver.getCurrentUrl(),"https://keybooks.ro/shop/");
+		
+		pag.SelectPriceLowToHigh(pag.dropdown);
+		
+		assertEquals(pag.CheckCurrentSelection(pag.dropdown),"Sort by price: low to high");
+		pag.Compare();
+	}
+
+}
