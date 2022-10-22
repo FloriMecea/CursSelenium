61f5f8273b619be8a57a70f1e9cb7c1c37f64255
Florentina Manescu <florentina.manescu@softelligence.net>
2022-10-23 00:52:52 +0300

    ''''''''''''''''' curs15 selenium

---
+++ src/main/java/pages/BlogPage2_Tema15.java
@@ -0,0 +1,48 @@
+package pages;
+
+import static org.testng.Assert.assertEquals;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.interactions.Actions;
+
+public class BlogPage2_Tema15 {
+	
+	public WebDriver driver;
+	public By audioPost= By.cssSelector("h4[class='post_title'] a[href='https://keybooks.ro/2016/02/02/audio-post/']");
+	public By sliderMelodie=By.cssSelector("span[class*='mejs-time-slider']");
+	public By clickPlay=By.cssSelector("div[class*='mejs-play']");
+	public By playPause= By.cssSelector("div[class*='playpause']");
+	public By sliderSonor=By.cssSelector("a[class*='volume-slider']");
+
+	
+	public BlogPage2_Tema15(WebDriver driver) {
+		this.driver=driver;
+	}
+	
+   public void hoverBlog() throws InterruptedException {
+		   NavMenuPage menu= new NavMenuPage(driver);
+		   menu.hoverElement(menu.blogLink);
+		   Thread.sleep(1000);
+		  
+   }
+	
+	public WebElement FindElement(By locator) {
+		   WebElement element=driver.findElement(locator);
+		  return element;
+			
+	}
+	public void addText(By locator, String message) {
+		   WebElement element=driver.findElement(locator);
+		   element.sendKeys(message);
+			
+	}
+	 public void dragAndDrop(By locator, int x, int y) {
+		   WebElement element= driver.findElement(locator);
+		   Actions action= new Actions(driver);
+		   
+		   action.dragAndDropBy(element, x, y).perform();
+	   }
+
+}
---
+++ src/main/java/tests/Actions_Tema15.java
@@ -0,0 +1,33 @@
+package tests;
+
+import static org.testng.Assert.assertEquals;
+
+import org.testng.annotations.Test;
+
+import pages.BlogPage2_Tema15;
+import pages.NavMenuPage;
+import utils.BaseTest;
+
+public class Actions_Tema15 extends BaseTest {
+	@Test (priority=0)
+	public void test() throws InterruptedException {
+		   NavMenuPage menu= new NavMenuPage(driver);
+		   menu.hoverElement(menu.blogLink);
+		   Thread.sleep(1000);
+		   menu.hoverElement(menu.blogLink);
+		   menu.hoverElement(menu.postFormatLogtLink);
+		   menu.navigateTo(menu.postFormatLogtLink);
+		   assertEquals(driver.getCurrentUrl(),"https://keybooks.ro/category/post-formats/");
+		   		   
+		   BlogPage2_Tema15 pag= new BlogPage2_Tema15(driver);
+		   menu.navigateTo(pag.audioPost);
+		   Thread.sleep(3000);
+		   assertEquals(driver.getCurrentUrl(),"https://keybooks.ro/2016/02/02/audio-post/");
+		   pag.FindElement(pag.clickPlay).click();
+		   Thread.sleep(6000);
+		   pag.FindElement(pag.playPause).click();
+		   pag.dragAndDrop(pag.sliderMelodie, 200, 0);
+		   Thread.sleep(3000);
+		   pag.dragAndDrop(pag.sliderSonor, -50, 0);
+	   }
+}
